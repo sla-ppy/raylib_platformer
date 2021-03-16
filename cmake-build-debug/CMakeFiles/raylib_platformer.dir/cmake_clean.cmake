87c3964a3b39c007d58512228d338e8e7ccb89b2
S4kyt <jerikolo1@gmail.com>
2021-03-16 11:41:35 +0100

    add clion project, cmake

---
+++ cmake-build-debug/CMakeFiles/raylib_platformer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raylib_platformer.dir/main.cpp.obj"
+  "raylib_platformer.exe"
+  "raylib_platformer.exe.manifest"
+  "raylib_platformer.lib"
+  "raylib_platformer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raylib_platformer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
