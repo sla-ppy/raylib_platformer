ebdb8919683e29459c3d2d4f68ed1ec4ff745b8b
S4kyt <jerikolo1@gmail.com>
2021-03-16 13:42:23 +0100

    add raylib dependency

--- cmake-build-debug/CMakeFiles/raylib_platformer.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/raylib_platformer.dir/DependInfo.cmake
@@ -8,12 +8,22 @@ set(CMAKE_DEPENDS_CHECK_CXX
   )
 set(CMAKE_CXX_COMPILER_ID "MSVC")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GRAPHICS_API_OPENGL_33"
+  "PLATFORM_DESKTOP"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../raylib/src"
+  "../raylib/src/external/glfw/include"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/User/source/repos/CLionRepos/raylib_platformer/cmake-build-debug/raylib/raylib/CMakeFiles/raylib.dir/DependInfo.cmake"
+  "C:/Users/User/source/repos/CLionRepos/raylib_platformer/cmake-build-debug/raylib/raylib/external/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -32,6 +32,11 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/User/source/repos/CLionRepos/raylib_platformer/cmake-build-debug/raylib/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
